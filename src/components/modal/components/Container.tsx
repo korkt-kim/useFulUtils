26212d6aaa3f4ffeab61023a8af5dc71cf9af3fe
korkt-kim <gnscott6@naver.com>
2024-04-02 15:32:39 +0000

    focusTrap 적용

--- src/components/modal/components/Container.tsx
+++ src/components/modal/components/Container.tsx
@@ -1,18 +1,21 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, forwardRef } from "react";
 
-export const Container = (props: PropsWithChildren) => {
-  return (
-    <div
-      style={{
-        zIndex: 1000,
-        position: "absolute",
-        inset: 0,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {props.children}
-    </div>
-  );
-};
+export const Container = forwardRef<HTMLDivElement, PropsWithChildren>(
+  function (props, ref) {
+    return (
+      <div
+        ref={ref}
+        style={{
+          zIndex: 1000,
+          position: "absolute",
+          inset: 0,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {props.children}
+      </div>
+    );
+  },
+);
